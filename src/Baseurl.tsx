eb9ba195ee1db42dc07b44277a200ba09f2e4fdb
Azadev <kodingo593@gmail.com>
2025-07-07 15:51:58 +0400

    .

--- src/Baseurl.tsx
+++ src/Baseurl.tsx
@@ -1,1 +1,2 @@
-export const endpoint = "https://kaiyi-21d4.onrender.com/api";
+// export const endpoint = "https://kaiyi-21d4.onrender.com/api";
+export const endpoint = "https://84.247.172.49/api/v2"
